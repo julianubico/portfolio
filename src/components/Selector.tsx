a079753a846832dad382a14e316e4f8ce217d0e8
d4julian <julianubico@gmail.com>
2025-01-30 13:22:44 -0500

    Update Selector.tsx

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -4,16 +4,16 @@ function Selector({ selected, setSelected }) {
   return (
     <div className="mx-auto flex w-full rounded-md border bg-white text-center text-black">
       <span
-        className={`w-full cursor-pointer rounded-md font-bold transition ${selected === "Education" ? "bg-slate-700 text-white" : "hover:bg-gray-400"}`}
-        onClick={() => setSelected("Education")}
-      >
-        Education
-      </span>
-      <span
         className={`w-full cursor-pointer rounded-md font-bold transition ${selected === "Work" ? "bg-slate-700 text-white" : "hover:bg-gray-400"}`}
         onClick={() => setSelected("Work")}
       >
         Work
+      </span>
+      <span
+        className={`w-full cursor-pointer rounded-md font-bold transition ${selected === "Education" ? "bg-slate-700 text-white" : "hover:bg-gray-400"}`}
+        onClick={() => setSelected("Education")}
+      >
+        Education
       </span>
     </div>
   );
